9256663d4744cdf0a90bc52be6937decb942997e
manik.kumar <manik.kumar@WS-HP-5CG6326SJ6.internal.utradesolutions.com>
2020-12-18 22:53:38 +0530

    Collections practice and brief info.

---
+++ selenium/src/seleniumclass/firstclass.java
@@ -0,0 +1,35 @@
+package seleniumclass;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firstclass {
+	
+	public static void main(String[] args) throws Exception 
+	{
+	    String weburl="https://www.facebook.com/";
+		System.setProperty("webdriver.chrome.driver", "../selenium/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get(weburl);
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+	    WebElement register = driver.findElement(By.id("u_0_2"));
+	    register.click();
+	    Thread.sleep(5000);
+	    WebElement fname = driver.findElement(By.id("u_1_b"));
+	    WebElement lname = driver.findElement(By.cssSelector("input[aria-label='Surname']"));
+	    WebElement email = driver.findElement(By.id("u_1_g"));
+	    WebElement passwd = driver.findElement(By.id("password_step_input"));
+        Thread.sleep(2000);
+	    fname.sendKeys("Manik");
+	    lname.sendKeys("Kumar");
+	    email.sendKeys("manikchandel24@gmail.com");
+	    passwd.sendKeys("hello_manik");
+	    Thread.sleep(8000);
+	    
+		driver.close();
+	}
+
+}
